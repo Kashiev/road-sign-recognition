f6e51667a966d5194a8c672e3c8b51d4d602c865
Murad <muradkarammaev@gmail.com>
2018-09-01 13:34:05 +0300

    Try to use 640x480 when possible

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import cv2
 from utils import Webcam
 
 cam = Webcam(sys.argv[1])
+
+# VGA is the best suited resolution for our task, try to set it
+if (640, 480) in cam.get_possible_resolutions():
+    cam.set_resolution(640, 480)
+
 while True:
     cv2.waitKey(1)
     frame = cam.next_frame()
